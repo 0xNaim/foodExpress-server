09a1d2f6312051799ad0e92d81611615d180fbb1
0xNaim <naimislam3545@gmail.com>
2022-10-15 02:31:04 +0600

    feat: foodexpress server setup

---
+++ config/database.js
@@ -0,0 +1,18 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "foodexpress"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Naim1234"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
+    },
+    debug: false,
+  },
+});
