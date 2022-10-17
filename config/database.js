68285d45873cbd7e6289016e4a0df1b953ba28ca
0xNaim <naimislam3545@gmail.com>
2022-10-18 05:00:57 +0600

    feat: product model added

--- config/database.js
+++ config/database.js
@@ -3,11 +3,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", "ec2-44-199-22-207.compute-1.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "foodexpress"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Naim1234"),
+      database: env("DATABASE_NAME", "d63c15emqhff21"),
+      user: env("DATABASE_USERNAME", "jwhjyyvyyzpvpm"),
+      password: env(
+        "DATABASE_PASSWORD",
+        "f0c64a4c7ca5d112343e1faad3b35f809b5eaf72d96937248c900357f34e38c3"
+      ),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
