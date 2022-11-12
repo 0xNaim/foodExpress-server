5a0a05e722b705721c053125e1551face8bc54c0
0xNaim <naimislam3545@gmail.com>
2022-11-12 20:10:51 +0600

    refactor: order model updated

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,5 +1,6 @@
 "use strict";
 const stripe = require("stripe")(process.env.STRIPE_SK);
+const shortid = require("shortid");
 
 /**
  * order controller
@@ -9,31 +10,29 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
-    const { products, amount, shippingAddress } = ctx.request.body;
-
     const { user } = ctx.state;
+    const { items, total, shippingAddress, token } = ctx.request.body;
 
-    const BASE_URL = ctx.request.headers.origin || "http://localhost:3000";
+    console.log(ctx.request.body);
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      customer_email: user.email,
-      mode: "payment",
-      success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: BASE_URL,
-      line_items: products,
+    const stripeAmount = Math.floor(amount * 100);
+    await stripe.charges.create({
+      // Transform cents to dollars
+      amount: stripeAmount,
+      currency: "usd",
+      source: token,
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
     });
 
-    // Create the order
-    const newOrder = await strapi.services.order.create({
-      user: user.id,
-      checkout_session: session.id,
-      items: products,
-      amount,
+    // Register the order in the database
+    const order = await strapi.services.order.create.create({
+      user: user.email,
+      order_id: shortid.generate(),
+      items,
+      total,
       shippingAddress,
-      status: "unpaid", 
     });
 
-    return { id: session.id };
+    return order;
   },
 }));
