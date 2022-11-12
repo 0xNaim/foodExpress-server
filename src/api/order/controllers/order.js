8d735738b09cfb61ab7b4fb8b7e15d84af218c00
0xNaim <naimislam3545@gmail.com>
2022-11-12 23:57:53 +0600

    feat: stripe payment implemented

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,11 +1,10 @@
 "use strict";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+const shortid = require("shortid");
 
 /**
  * order controller
  */
-
-const stripe = require("stripe")(process.env.STRIPE_SK);
-const shortid = require("shortid");
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
@@ -13,25 +12,29 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
     const { user } = ctx.state;
     const { items, amount, shippingAddress, token } = ctx.request.body;
 
-    await stripe.charges.create({
-      // Transform cents to dollars
-      amount,
-      currency: "usd",
-      source: token,
-      description: `Order ${new Date()} by ${ctx.state.user._id}`,
-    });
-
-    // Register the order in the database
-    const order = await strapi.db.query("api::order.order").create({
-      data: {
-        user: user.id,
-        order_id: shortid.generate(),
-        items,
+    try {
+      await stripe.charges.create({
+        // Transform cents to dollars
         amount,
-        shippingAddress,
-      },
-    });
+        currency: "usd",
+        source: token,
+        description: `Order ${new Date()} by ${user.email}`,
+      });
 
-    return order;
+      // Register the order in the database
+      const order = await strapi.db.query("api::order.order").create({
+        data: {
+          user: user.id,
+          order_id: shortid.generate(),
+          items,
+          amount,
+          shippingAddress,
+        },
+      });
+
+      return order;
+    } catch (error) {
+      return error;
+    }
   },
 }));
