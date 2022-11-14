cc5a23e19aff715d1a5aa3570f52cf8b91cfd668
0xNaim <naimislam3545@gmail.com>
2022-11-14 17:07:38 +0600

    refactor: secrect credentials removed

--- config/database.js
+++ config/database.js
@@ -3,14 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "ec2-44-199-22-207.compute-1.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "d63c15emqhff21"),
-      user: env("DATABASE_USERNAME", "jwhjyyvyyzpvpm"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "f0c64a4c7ca5d112343e1faad3b35f809b5eaf72d96937248c900357f34e38c3"
-      ),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
